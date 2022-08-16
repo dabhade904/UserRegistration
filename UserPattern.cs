b908d69dbcac776705081d5a35d0d8541c8e85ed
dabhade904 <dabhade904@gmail.com>
2022-08-17 01:47:06 +0530

    Added : Fuction to check last name is valid or not based on Regex Pattern

--- UserPattern.cs
+++ UserPattern.cs
@@ -4,18 +4,18 @@ namespace UserRegistrationProblem
 {
     public class UserPattern
     {
-        public static string F_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
+        public static string F_L_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
 
         public static void getFullNamePattern(string pattern)
         {
-            var match = Regex.IsMatch(F_Name_Pattern, pattern);
+            var match = Regex.IsMatch(F_L_Name_Pattern, pattern);
             if (match)
             {
-                Console.WriteLine("First name is valid");
+                Console.WriteLine("Name is valid");
             }
             else
             {
-                Console.WriteLine("Not Valid ,First Name Should start with Caps and has minimun 3 characters");
+                Console.WriteLine("Not Valid , Name Should start with Caps and has minimun 3 characters");
             }
         }
     }
