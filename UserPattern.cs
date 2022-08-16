99d0b58ebc1a47db837ca732ecb8224a34872702
dabhade904 <dabhade904@gmail.com>
2022-08-17 02:14:32 +0530

    Added : pattern to check mobile number is valid or not

--- UserPattern.cs
+++ UserPattern.cs
@@ -6,6 +6,7 @@ namespace UserRegistrationProblem
     {
         public static string F_L_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
         public static string Email_ID_Pattern = "^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$";
+        public static string Mobile_Pattern = "^[0-9]{2,2}[ ][7,8,9]{1,1}[0-9]{9}$";
         public static void getFullNamePattern(string pattern)
         {
             var match = Regex.IsMatch(pattern, F_L_Name_Pattern);
@@ -28,7 +29,20 @@ namespace UserRegistrationProblem
             }
             else
             {
-                Console.WriteLine("Not Valid ");
+                Console.WriteLine("Email ID is not valid ");
+            }
+        }
+
+        public static void getMobileNumber(string mobile)
+        {
+            var match = Regex.IsMatch(mobile, Mobile_Pattern);
+            if (match)
+            {
+                Console.WriteLine("Mobile number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Mobile number is not valid ");
             }
         }
     }
